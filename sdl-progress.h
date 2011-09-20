06473e7c4d67ccd21961fc56732692781c4bf2fd
Sean Cross <sean@chumby.com>
2011-09-20 17:00:33 +0800

    sdl-progress: Add header file

---
+++ sdl-progress.h
@@ -0,0 +1,8 @@
+#ifndef __SDL_PROGRESS_H__
+#define __SDL_PROGRESS_H__
+#include <SDL/SDL.h>
+#include "progress.h"
+
+void redraw_progress(struct progress *picker, SDL_Surface *screen);
+
+#endif /* __SDL_PROGRESS_H__ */
